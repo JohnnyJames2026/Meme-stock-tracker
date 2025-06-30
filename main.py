75a194efda88178b7f0fcd86b3683ae4c971d048
Jonathan James <jjon1883athan@gmail.com>
2025-06-29 21:06:38 -0500

    Making adjustments on get_recent_news() because some of the news has different library names. Still needs fixing.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
+from stock import Stock
+
+pltr = Stock("PLTR")
+
+print(pltr)
--- stock.py
+++ stock.py
@@ -16,25 +16,34 @@ class Stock:
 
 
     def get_stock_data(self):
+        #returns date, close price, day's high, day's low, open price, volume
         return yf.download(self.ticker, self.start_date, self.current_date)
     
     
     def get_recent_news(self):
         ticker = yf.Ticker(self.ticker)
-        raw_news = ticker.get_news()
+        raw_news = ticker.get_news(count = 5)
         article_list = []
         for article in raw_news:
-            article_list.append(
-                {"Title": article.get("content").get("title"),
-                 "Summary": article.get("content").get("summary"), 
-                 "url": article.get("content").get("clickThroughUrl").get("url")
-                 })
+            if article["content"]: 
+                article_list.append(
+                    {"Title": article.get("content").get("title"),
+                    "Summary": article.get("content").get("summary"), 
+                    "url": article.get("content").get("clickThroughUrl").get("url")
+                    })
+            else: 
+                article_list.append(
+                    {"Title": article.get("title"),
+                    "Summary": article.get("summary"), 
+                    "url": article.get("clickThroughUrl").get("url")
+                    })
         return article_list
 
 
 
 
 if __name__ == "__main__":
-    test_stock = Stock("AAPL")
+    test_stock = Stock("PLTR")
     print(test_stock.data)
-    print(test_stock.news)
+    print(test_stock.get_recent_news())
+    
